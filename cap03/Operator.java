8a5bf6c9610e61382176f0c4daf3c1902ddac522
David Gesrob <david91br@gmail.com>
2020-06-21 20:11:25 -0300

    Cap03 - Operator

---
+++ cap03/Operator.java
@@ -0,0 +1,15 @@
+public class Operator {
+
+    public static void main(String[] args) {
+
+	boolean a = true && false;
+	boolean b = false && true;
+	
+	System.out.println(a);
+	System.out.println(b);
+    }
+}
+
+	
+
+	
